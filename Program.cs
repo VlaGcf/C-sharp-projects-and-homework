c2022cd54b0f3c338fd9f49102e9abfe868e68b5
Vlad Lazarev <59767514+VlaGcf@users.noreply.github.com>
2021-10-24 20:23:14 +0300

    Вычислить факториал числа p

--- Program.cs
+++ Program.cs
@@ -1,20 +1,21 @@
 ﻿using System;
 
-namespace Вывести_таблицы_умножения_от_второй_до_шестой
+namespace Вычислить_факториал_числа_p
 {
     class Program
     {
         static void Main(string[] args)
         {
-            int i = 0;
-            int j = 0;
-            for (i = 2; i < 7; i++)
-            {
-                for (j = 2; j<7; j++)
-                    Console.Write(i*j);
-                Console.WriteLine();
-            }
-
+            int s = 1;
+            int k = 1;
+            int p=Convert.ToInt32(Console.ReadLine());
+                do
+                {
+                    s = s * k;
+                    k ++;
+                } while (k!=p+1);
+                Console.WriteLine(s);
+            
         }
     }
 }
